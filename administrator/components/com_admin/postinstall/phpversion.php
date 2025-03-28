f62dac45adeacc2677b770443508a44d2d644bc5
usu2068 <administrador@ustarizabogados.com>
2025-03-28 08:39:51 -0500

    postinstall phpversion.php documentado - KP

--- administrator/components/com_admin/postinstall/phpversion.php
+++ administrator/components/com_admin/postinstall/phpversion.php
@@ -7,18 +7,23 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  *
  * This file contains post-installation message handling for the checking minimum PHP version support
+ * Este archivo contiene la gestion de mensajes posteriores a la instalación para verificar la compatibilidad con la version minima de PHP requerida.
  */
 
+ //evita el acceso directo al archivo
 defined('_JEXEC') or die;
 
 /**
  * Checks if the PHP version is less than 5.3.10.
+ * comprueba si la version de PHP es inferiror a 5.3.10
  *
- * @return  integer
+ * @return  integer //retorna 1 si la version de PPHP es menor que 5.3.10, de lo contrario, retorna 0
  *
  * @since   3.2
  */
 function admin_postinstall_phpversion_condition()
 {
-	return version_compare(PHP_VERSION, '5.3.10', 'lt');
+	return version_compare(PHP_VERSION, '5.3.10', 'lt'); //compara la version de PHP con la minima requerida
 }
+
+//Nota: se alerta a los administradores si su servidor no cumple con los requisitos minimos de PHP.
